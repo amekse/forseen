be1e3d658d8f49ac2c2bf9500890f4568e4416fc
Saswata Das <dassaswata1996@gmail.com>
2024-11-02 17:19:40 +0530

    minor algo change

--- src/services/optimisedProjection.service.ts
+++ src/services/optimisedProjection.service.ts
@@ -7,6 +7,7 @@ TODO:
 4A. if no extra months are given with budget we keep adding each new month with budget as average from previous budget list and adjust the extra amount there
 4B. if extra months given with budget we adjust there
 5. next we search for the costliest / least priority item in the expenses list and put them to delay list
-6. check if extra amount crosses total amount from delay list, we search for the next priority/costlier item and add that to delay
-7. check the delay list for delay dates and show user
+6. check if extra amount crosses total amount from delay list, we search for the next higher/lesser/same priority/cost item and add that to delay
+7. check the delay list for which month covers the cost for the highest priority or lesser cost item and continue for all in the list
+8. show user
 */
